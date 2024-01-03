7971044e5af6ff5305d6d8e02b0183ffec301702
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-04 06:54:59 +0800

    added framer motion deps and styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './app.scss';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
     <>
-      <section>Hero</section>
+      <section>
+        <Navbar />
+      </section>
       <section>Parallax</section>
       <section>Services</section>
       <section>Parallax</section>
