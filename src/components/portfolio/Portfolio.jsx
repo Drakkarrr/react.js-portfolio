8dd4a9e1c49a3a1f2d5015340486747407a47958
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-05 18:37:05 +0800

    finish projects showcase

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,27 +5,24 @@ import { motion, useScroll, useSpring, useTransform } from 'framer-motion';
 const items = [
   {
     id: 1,
-    title: 'React Commerce',
-    img: 'https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    title: 'D-prompt-AI',
+    img: 'https://github.com/Drakkarrr/D-prompt-AI/raw/main/public/ui-screenshot/landing-page.png',
+    link: 'https://d-prompt-ai.vercel.app/',
+    desc: 'An Open source Generative AI web-based app, an unprecedented platform that harnesses the boundless potential of artificial intelligence to ignite your creative spark!',
   },
   {
     id: 2,
-    title: 'Next.js Blog',
-    img: 'https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    title: 'Izteem gamehub',
+    img: '../public/izteem.png',
+    link: 'https://izteem-gamehub.vercel.app/',
+    desc: 'A simple game marketplace platform built with react and TypeScript for UI and rawg API.',
   },
   {
     id: 3,
-    title: 'Vanilla JS App',
-    img: 'https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
-  },
-  {
-    id: 4,
-    title: 'Music App',
-    img: 'https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    title: 'Bookscrape',
+    img: 'https://github.com/Drakkarrr/booking.com-clone/raw/main/screenshots/landing.png',
+    link: 'https://booking-clone-beta.vercel.app/',
+    desc: 'A web-based flight booking app and Booking.com clone that scrapes its real-time data to provide flights.',
   },
 ];
 
@@ -43,7 +40,7 @@ const Single = ({ item }) => {
       <div className='container'>
         <div className='wrapper'>
           <div className='imageContainer' ref={ref}>
-            <img src={item.img} alt='' />
+            <img style={{ objectFit: 'fill' }} src={item.img} alt='' />
           </div>
           <motion.div className='textContainer' style={{ y }}>
             <h2>{item.title}</h2>
